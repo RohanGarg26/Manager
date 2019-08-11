0e290551d7deb5a612760c6814afdc81f1d0c7bd
RohanGarg26 <garg.rohan26@gmail.com>
2019-08-12 01:15:24 +0530

    changed routes and added edit option

--- controllers/company-admin.js
+++ controllers/company-admin.js
@@ -14,80 +14,9 @@ const transporter = nodemailer.createTransport(sgTransport({
   }
 }))
 
+
 //members
-exports.getPostMember = (req, res, next) => {
-  if (req.method == "POST") { //if request is post i.e. user has addaed new member
-    const pass = password.randomPassword({ length: 10, characters: [password.lower, password.upper, password.digits, password.symbols] })
-    bcrypt.hash(pass, 12)
-      .then(hashedPass => {
-        return Promise.all([Team.findOne({
-          $and: [
-            { team: req.body.team },
-            { companyId: req.params.compId }
-          ]
-        }), hashedPass])  //finding the teams to be displayed in the views for selection
-      })
-      .then(([team, hashedPass]) => {
-        const image = req.file
-        if (req.body.team != 'Team Not Assigned') {  //if a team was selected in the form
-          return new Member({  //creating a new document with details added by the user in the forn
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            dob: req.body.dob,
-            address: req.body.address,
-            emailId: req.body.emailId,
-            imageUrl: image.path,
-            jobTitle: req.body.jobTitle,
-            jobDesc: req.body.jobDesc,
-            team: req.body.team,
-            companyId: req.params.compId,
-            teamId: team._id,
-            teamHead: req.body.teamHead,
-            password: hashedPass
-          }, { versionKey: false }).save()
-        }
-        else {  //if a team was not selected in the form
-          return new Member({
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            dob: req.body.dob,
-            address: req.body.address,
-            emailId: req.body.emailId,
-            imageUrl: image.path,
-            jobTitle: req.body.jobTitle,
-            jobDesc: req.body.jobDesc,
-            team: req.body.team,
-            companyId: req.params.compId,
-            teamId: new mongoose.Types.ObjectId('000000000000000000000000'),
-            teamHead: req.body.teamHead,
-            password: hashedPass
-          }, { versionKey: false }).save()
-        }
-      })
-      .then(member => {
-        return transporter.sendMail({
-          to: 'garg.rohan26@gmail.com',
-          from: 'Manager',
-          subject: 'Welcome to the Manager',
-          html: `
-            <body>
-            <p>Hello ${ req.body.firstName}</p>
-            <p>You've been added to Manager by your company</p>
-            <p>
-              Your login credentials are: <br>
-              Email: ${ req.body.emailId} <br>
-              Password: ${ pass} <br>
-                 <small>Store your credentials in a safe place.</small>
-            </p>
-            <p>Have a great experience!!!</p>
-            </body>
-            `
-        })
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+exports.getMember = (req, res, next) => {
   if (req.query.searchField) {  //if user uses the search bar for finding a member
     const search = req.query.searchField
     const firstN = search.split(' ')[0]
@@ -152,11 +81,90 @@ exports.getPostMember = (req, res, next) => {
   }
 }
 
+exports.postAddMember = (req, res, next) => {
+  const image = req.file
+  const pass = password.randomPassword({ length: 10, characters: [password.lower, password.upper, password.digits, password.symbols] })
+  bcrypt.hash(pass, 12)
+    .then(hashedPass => {
+      return Promise.all([Team.findOne({
+        $and: [
+          { team: req.body.team },
+          { companyId: req.params.compId }
+        ]
+      }), hashedPass])  //finding the teams to be displayed in the views for selection
+    })
+    .then(([team, hashedPass]) => {
+      if (req.body.team != 'Team Not Assigned') {  //if a team was selected in the form
+        return new Member({  //creating a new document with details added by the user in the forn
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          dob: req.body.dob,
+          address: req.body.address,
+          emailId: req.body.emailId,
+          imageUrl: image.path,
+          jobTitle: req.body.jobTitle,
+          jobDesc: req.body.jobDesc,
+          team: req.body.team,
+          companyId: req.params.compId,
+          teamId: team._id,
+          teamHead: req.body.teamHead,
+          password: hashedPass
+        }, { versionKey: false }).save()
+      }
+      else {  //if a team was not selected in the form
+        return new Member({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          dob: req.body.dob,
+          address: req.body.address,
+          emailId: req.body.emailId,
+          imageUrl: image.path,
+          jobTitle: req.body.jobTitle,
+          jobDesc: req.body.jobDesc,
+          team: req.body.team,
+          companyId: req.params.compId,
+          teamId: new mongoose.Types.ObjectId('000000000000000000000000'),
+          teamHead: req.body.teamHead,
+          password: hashedPass
+        }, { versionKey: false }).save()
+      }
+    })
+    .then(member => {
+      return transporter.sendMail({
+        to: 'garg.rohan26@gmail.com',
+        from: 'Manager',
+        subject: 'Welcome to the Manager',
+        html: `
+            <body>
+            <p>Hello ${ req.body.firstName}</p>
+            <p>You've been added to Manager by your company</p>
+            <p>
+              Your login credentials are: <br>
+              Email: ${ req.body.emailId} <br>
+              Password: ${ pass} <br>
+                 <small>Store your credentials in a safe place.</small>
+            </p>
+            <p>Have a great experience!!!</p>
+            </body>
+            `
+      })
+    })
+    .then(() => {
+      res.redirect('/' + encodeURIComponent(req.params.compId) + '/members')
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
 //add-member
 exports.getAddMember = (req, res, next) => {
   Team.find() //finding teams that exist so that they can be selected in the form
     .then(team => {
-      res.render('add-member', { team: team, path: ' ', cId: req.params.compId })
+      res.render('add-member', { team: team, path: ' ', cId: req.params.compId, edit: 'false' })
+    })
+    .catch(err => {
+      console.log(err)
     })
 }
 
@@ -186,18 +194,7 @@ exports.getMemberDetails = (req, res, next) => {
 }
 
 //teams
-exports.getPostTeam = (req, res, next) => {
-  if (req.method == "POST") { //if a post request is made by submitting the form to ad a new team
-    const image = req.file
-    new Team({
-      team: req.body.team,
-      teamDesc: req.body.teamDesc,
-      imageUrl: image.path,
-      companyId: req.params.compId,
-      teamId: req.body.teamId
-    }, { versionKey: false })
-      .save()
-  }
+exports.getTeam = (req, res, next) => {
   if (req.query.searchField) {  //if a request is made using the search bar by the user
     const search = req.query.searchField
     Team.find({
@@ -229,7 +226,7 @@ exports.getPostTeam = (req, res, next) => {
         console.log(err)
       })
   }
-  else {  //if either a get request is made or new team is added
+  else {
     Team.countDocuments({ companyId: req.params.compId })
       .then(count => {
         if (count == 0) {
@@ -250,6 +247,24 @@ exports.getPostTeam = (req, res, next) => {
       })
   }
 }
+
+exports.postAddTeam = (req, res, next) => {
+  const image = req.file
+  new Team({
+    team: req.body.team,
+    teamDesc: req.body.teamDesc,
+    imageUrl: image.path,
+    companyId: req.params.compId,
+    teamId: req.body.teamId
+  }, { versionKey: false })
+    .save()
+    .then(team => {
+      res.redirect('/' + encodeURIComponent(req.params.compId) + '/teams')
+    })
+}
+
+
+
 
 //team-details
 exports.getTeamDetails = (req, res, next) => {
@@ -288,7 +303,14 @@ exports.getAddTeam = (req, res, next) => {
 
 //delete-member
 exports.deleteMem = (req, res, next) => {
-  Member.deleteOne({ _id: req.params.memberId })
+  Member.findOne({ _id: req.params.memberId })
+    .then(member => {
+      fs.unlink(member.imageUrl, (err) => {
+        console.log(err)
+      })
+      return Member.deleteOne({ _id: req.params.memberId })
+    })
+
     .then(() => {
       res.redirect('/' + encodeURIComponent(req.params.compId) + '/members')
     })
@@ -299,15 +321,14 @@ exports.deleteMem = (req, res, next) => {
 
 //delete-team
 exports.deleteTeam = (req, res, next) => {
-  Member.find({ teamId: req.params.teamId })
-    .then(member => {
-      for (let m of member) {
-        fs.unlink(m.imageUrl, (err) => {
-          console.log(err)
-        })
-      }
-      return Member.deleteMany({ teamId: req.params.teamId })
-    })
+  Member.updateMany(
+    { teamId: req.params.teamId },
+    {
+      team: 'Team Not Assigned',
+      teamHead: 'No',
+      teamId: new mongoose.Types.ObjectId('000000000000000000000000')
+    }
+  )
     .then(() => {
       return Team.findOne({ _id: req.params.teamId })
     })
@@ -323,4 +344,98 @@ exports.deleteTeam = (req, res, next) => {
     .catch(err => {
       console.log(err)
     })
+}
+
+//edit-member
+exports.getEditMember = (req, res, next) => {
+  Team.find() //finding teams that exist so that they can be selected in the form
+    .then(team => {
+      return Promise.all([Member.findOne({ _id: req.params.memberId }), team])
+    })
+    .then(([member, team]) => {
+      res.render('add-member', { team: team, path: ' ', cId: req.params.compId, edit: 'true', member: member })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+exports.postEditMember = (req, res, next) => {
+  const image = req.file
+  if (req.body.team != 'Team Not Assigned') {
+    Member.findById(req.body.memberId)
+      .then(member => {
+        if (image) {
+          fs.unlink(member.imageUrl, (err) => {
+            console.log(err)
+          })
+        }
+      })
+      .then(() => {
+        return Team.findOne({
+          $and: [
+            { team: req.body.team },
+            { companyId: req.params.compId }
+          ]
+        })
+      })
+      .then(team => {
+        return Member.updateOne(
+          { _id: req.body.memberId },
+          {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            dob: req.body.dob,
+            address: req.body.address,
+            emailId: req.body.emailId,
+            imageUrl: image.path,
+            jobTitle: req.body.jobTitle,
+            jobDesc: req.body.jobDesc,
+            team: req.body.team,
+            teamId: team._id,
+            teamHead: req.body.teamHead
+          }
+        )
+      })
+      .then(() => {
+        res.redirect('/' + encodeURIComponent(req.params.compId) + '/members')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+  else {
+    Member.findById(req.body.memberId)
+      .then(member => {
+        if (image) {
+          fs.unlink(member.imageUrl, (err) => {
+            console.log(err)
+          })
+        }
+      })
+      .then(() => {
+        return Member.updateOne(
+          { _id: req.body.memberId },
+          {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            dob: req.body.dob,
+            address: req.body.address,
+            emailId: req.body.emailId,
+            imageUrl: image.path,
+            jobTitle: req.body.jobTitle,
+            jobDesc: req.body.jobDesc,
+            team: req.body.team,
+            teamId: new mongoose.Types.ObjectId('000000000000000000000000'),
+            teamHead: req.body.teamHead
+          }
+        )
+      })
+      .then(() => {
+        res.redirect('/' + encodeURIComponent(req.params.compId) + '/members')
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
 }
--- routes/company-admin.js
+++ routes/company-admin.js
@@ -4,16 +4,20 @@ const router = express.Router()
 const adminController = require('../controllers/company-admin')
 const authMiddleware = require('../middlewares/isAuth')
 
-router.get('/:compId/members/:memberId',authMiddleware.isAuth ,adminController.getMemberDetails)
-router.use('/:compId/members',authMiddleware.isAuth ,adminController.getPostMember)
+router.get('/:compId/member/edit-member/:memberId',authMiddleware.isAuth ,adminController.getEditMember)
+router.post('/:compId/member/edit-member',authMiddleware.isAuth ,adminController.postEditMember)
 router.post('/:compId/member/delete/:memberId',authMiddleware.isAuth ,adminController.deleteMem)
-
-router.get('/:compId/teams/:teamId',authMiddleware.isAuth ,adminController.getTeamDetails)
-router.use('/:compId/teams',authMiddleware.isAuth ,adminController.getPostTeam)
-router.post('/:compId/team/delete/:teamId',authMiddleware.isAuth ,adminController.deleteTeam)
-
+router.get('/:compId/members/:memberId',authMiddleware.isAuth ,adminController.getMemberDetails)
+router.get('/:compId/members',authMiddleware.isAuth ,adminController.getMember)
+router.post('/:compId/add-member',authMiddleware.isAuth ,adminController.postAddMember)
 router.get('/:compId/add-member',authMiddleware.isAuth ,adminController.getAddMember)
+
+
+router.post('/:compId/team/delete/:teamId',authMiddleware.isAuth ,adminController.deleteTeam)
+router.get('/:compId/teams/:teamId',authMiddleware.isAuth ,adminController.getTeamDetails)
+router.get('/:compId/teams',authMiddleware.isAuth ,adminController.getTeam)
 router.get('/:compId/add-team',authMiddleware.isAuth ,adminController.getAddTeam)
+router.post('/:compId/add-team',authMiddleware.isAuth ,adminController.postAddTeam)
 
 
 exports.routes = router
